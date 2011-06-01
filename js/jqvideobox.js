c8e75cc630c928039b889f0812872199c43e9844
unknown <maxim.voldachinsky@.ewave.co.il>
2011-06-01 11:36:42 +0300

    readme update

--- js/jqvideobox.js
+++ js/jqvideobox.js
@@ -1,28 +1,10 @@
-﻿/*
- jqVideoBox 1.52
+﻿/**
+ jqVideoBox 1.52 is released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  - Jquery version required: 1.2.x - 1.6.0
  - SWFObject version required: v2.x
+*/
 
- Changelog:
- - 1.5.2 added support for gametrailers.com, myvideo.de, collegehumor.com, sevenload.com
- 
- - 1.5.1 added support for smotri.com, vkontakte.ru(hash needed), rutube.ru, video.mail.ru, video.qip.ru
-   thumbl.in support added, fetching thumbnails from thumbl.in
-   
- - 1.5 added support for dailymotion.com, blip.tv, revver.com, veoh.com, vimeo.com
-   added paging in specific group
-   some minor fixes
-   swfobject 2.x support added
-  
- - 1.00 ported from mootools plugin videbox (http://videobox-lb.sourceforge.net/) to jquery
- */
-
-/* Coded by: emposha <admin@emposha.com> */
-/* Copyright: Emposha.com <http://www.emposha.com/> - Distributed under MIT - Keep this message! */
-
-/*
- * Main idea and concept mootools plugin videbox (http://videobox-lb.sourceforge.net/)
- */
+/* Main idea and concept mootools plugin videbox <http://videobox-lb.sourceforge.net/> */
 
 /*
  * width               - Width of the lightbox
